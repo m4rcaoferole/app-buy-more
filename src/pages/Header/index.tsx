a0b97ed5dc84384a859c3c4fdcb9d9e7bea3cbd3
Marcos Vinicius <marcos.oliveira@liberali.com.br>
2022-05-28 10:27:13 -0400

    fix: implementar o hook usecontext

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,29 +1,23 @@
-import cart from "../../assets/cart.svg"
+import cart from "../../assets/cart.svg";
 
-import "./styles.scss"
-
+import "./styles.scss";
 export interface INumberCartProps {
   quantityProduct?: number
 }
 
 export const Header = ({ quantityProduct }: INumberCartProps) => {
-
   return (
     <div className="container-header">
       <div className="title-wrapper">
         <h1>BuyMore</h1>
       </div>
 
-      <div className="cart-container">        
-          <div className="cart-wrapper">
-            <img src={cart} alt="Carrinho de compras" />
-            <div 
-              className="circle-container"
-            >
-              {quantityProduct}
-            </div>
-          </div>
+      <div className="cart-container">
+        <div className="cart-wrapper">
+          <img src={cart} alt="Carrinho de compras" />
+          <div className="circle-container">{quantityProduct}</div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
 import { Header, INumberCartProps } from "../Header/index";
 import { ListProducts } from "../../types/products";
-import cartmini from "../../assets/cartmini.svg"
+import cartmini from "../../assets/cartmini.svg";
 
 import { api } from "../../services/api";
 
 import "./styles.scss";
 
-export const Home: React.FC = () => {
+export const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ListProducts[]>([]);
-  const [addCart, setAddCart] = useState<INumberCartProps>()
+  const [addCart, setAddCart] = useState<INumberCartProps>();
 
   async function fetchProducts() {
-   try {
-      const response = await api.get("/products?limit=2")
-      setProducts(response.data)
-   } catch (error) {
-      console.log(error)
-   }
+    try {
+      const response = await api.get("/products?limit=2");
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function handleAddCart(product: ListProducts) {
-    console.log(product,'Produto clicado')
+    console.log(product, "Produto clicado");
   }
 
   useEffect(() => {
@@ -31,38 +31,36 @@ export const Home: React.FC = () => {
   return (
     <div className="container">
       <div className="title">
-       <Header />
+        <Header />
       </div>
       <div className="products-container">
         {products.map((product) => (
-          <div className="card-container">
+          <div key={product.id} className="card-container">
             <div className="figure-container">
               <img src={product.image} alt="Imagem do produto" />
             </div>
-    
+
             <div className="content-container">
-              <div  className="title-product">
-                <strong>
-                  {product.title}
-                </strong>
+              <div className="title-product">
+                <strong>{product.title}</strong>
                 <p>{product.description}</p>
               </div>
-    
+
               <div className="wrapper-container">
                 <div className="value-product">
                   <div>****</div>
                   <p>R$ {product.price}</p>
                 </div>
-    
+
                 <div className="btn-container">
                   <button onClick={() => handleAddCart(product)}>
-                    <img src={cartmini} alt="Carrinho do botão"/>
+                    <img src={cartmini} alt="Carrinho do botão" />
                     <span>Add Cart</span>
                   </button>
                 </div>
-              </div>              
-            </div>          
-          </div>        
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
